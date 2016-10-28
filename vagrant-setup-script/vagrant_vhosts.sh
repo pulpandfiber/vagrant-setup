8ed11e12173346c82a6c1415d0fa99569af32a70
Community <shaque@fiberdesign.ca>
2016-10-28 15:22:00 -0400

    Update vagrant_vhosts.sh

--- vagrant-setup-script/vagrant_vhosts.sh
+++ vagrant-setup-script/vagrant_vhosts.sh
@@ -1,4 +1,6 @@
 cp -R /var/www/vagrant-sites-config/* /etc/apache2/sites-available
-a2ensite /etc/apache2/sites-available/local.PROJECT_NAME.com.conf
-a2ensite /etc/apache2/sites-available/phpmyadmin.PROJECT_NAME.com.conf
+cd /etc/apache2/sites-available
+a2ensite local.PROJECT_NAME.com.conf
+a2ensite phpmyadmin.PROJECT_NAME.com.conf
+service apache2 reload
 service apache2 restart
